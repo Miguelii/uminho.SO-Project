3163c1ead397bf01d1a96ccdfe7ee1338643cb9c
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2022-03-16 22:06:13 +0000

    Base

---
+++ src/sdstored.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h> 
+#include <sys/wait.h>
+
+// SERVER
+
+int main(int argc, char *argv[]) {
+    if(argc < 3) perror("falta argumentos ");
+}
